c53b4b023a31df6397bdaa1e51fce4699ad9fc3b
mathct <mathieu.chatrain@gmail.com>
2025-01-26 19:29:05 +0100

    progression

--- modules/php/Game.php
+++ modules/php/Game.php
@@ -248,21 +248,24 @@ public function getGameProgression()
         $count[] = count(self::getObjectListFromDB( "SELECT card_id id FROM cards WHERE card_location_arg = '{$player}' AND card_visible = 1", true ));
     }
 
-    $min = min($count);
+    //$min = min($count);
 
     if ($nbreplayers == 2)
     {
-        return floor(2*$min * 100 / 16);
+        //return floor(2*$min * 100 / 16);
+        return floor(($count[0]+$count[1]) * 100 / 16);
     }
 
     if ($nbreplayers == 3)
     {
-        return floor(3*$min * 100 / 24);
+        //return floor(3*$min * 100 / 24);
+        return floor(($count[0]+$count[1]+$count[2]) * 100 / 24);
     }
 
     if ($nbreplayers == 4)
     {
-        return floor(4*$min * 100 / 32);
+        //return floor(4*$min * 100 / 32);
+        return floor(($count[0]+$count[1]+$count[2]+$count[3]) * 100 / 32);
     }
 
 
